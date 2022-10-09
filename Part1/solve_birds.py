ed3124341584ca8bd97ad5c5130b285726c190b1
tmahind <tmahind@iu.edu>
2022-10-08 23:49:47 -0400

    Improvements for Part1

--- Part1/solve_birds.py
+++ Part1/solve_birds.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 # solve_birds.py : Bird puzzle solver
 #
-# Code by: Dhanush Bharath Raj dbraj
+# Code by: TANMAY GIRISH MAHINDRAKAR: TMAHIND, DHANUSH BHARATH RAJ: DBRAJ
 #
 # Based on skeleton code by D. Crandall & B551 course staff, Fall 2022
 #
@@ -12,6 +12,7 @@
 
 # !/usr/bin/env python3
 import sys
+from queue import PriorityQueue
 
 N=5
 
@@ -25,33 +26,34 @@ N=5
 def is_goal(state):
     return state == list(range(1, N+1))
 
+def give_goal_state():
+    return list(range(1,N+1))
+
 # Successor function:
 # given a state, return a list of successor states
 def successors(state):
     return [ state[0:n] + [state[n+1],] + [state[n],] + state[n+2:] for n in range(0, N-1) ]
 
-#To Compare the Distances between the current and the goal state
+# Heuristic function:
+# given a state, return an estimate of the number of steps to a goal from that state
+
 def goal_state_pos(element):
-    
-    goal = [1,2,3,4,5]
+    goal = give_goal_state()
     for i in range(len(goal)):
         if(element == goal[i]):
             return i
            
     return -1
 
-
-# Heuristic function:
-# given a state, return an estimate of the number of steps to a goal from that state
 def h(state):
-    
-    huristic_value = 0
+    heuristic_value = 0
     for i in range(len(state)):
         
         #Calculating the huristic cost value by counting the total number of misplaced birds
-        huristic_value += (abs(i - goal_state_pos(state[i])))
+        heuristic_value += (abs(i - goal_state_pos(state[i])))
            
-    return huristic_value
+    return heuristic_value
+
 
 #########
 #
@@ -60,48 +62,21 @@ def h(state):
 # This is a generic solver using BFS. 
 #
 def solve(initial_state):
-    fringe = []
-    
-    #Initializing some Constant variables 
-    least = 99999
-    prior_state = 0
-    
-    #Initializing the fringe with the state, path and Cost till that state
-    fringe += [(initial_state, [], 0),]
-    
-    
-    while len(fringe) > 0:
-        
-        #Popping the fringe with the maximum priority that is minimum cost value 
-        (state, path, cost) = fringe.pop(prior_state)
-        
-        for i in range(len(fringe)):
-            
-            #Identifying the state in the fringe with least cost value and initializing it as the most prioritized one 
-            (state_i,path_i,cost_i) = fringe[i]
-            
-            if(cost_i < least):
-                least = cost_i
-                prior_state = i
-            
-        
+    fringe =  PriorityQueue()
+    fringe.put((0, initial_state, []))
+    while fringe:
+        (heuristic, state, path) = fringe.get()
         if is_goal(state):
             return path+[state,]
-
         for s in successors(state):
-            
-            #Calculating the cost function that is f(x) = h(x) + g(x)
-            cost = len(state) + h(state)
-            
-            #Appending all the possible successor states to the fringe
-            fringe.append((s, path+[state,], cost))
-            
+            fringe.put((h(s), s, path+[state,]))
+            #print(fringe)
 
     return []
 
 # Please don't modify anything below this line
 #
-if __name__ == '__main__':
+if __name__ == "__main__":
     if(len(sys.argv) != 2):
         raise(Exception("Error: expected a test case filename"))
 
@@ -110,4 +85,11 @@ if __name__ == '__main__':
         for line in file:
             test_cases.append([ int(i) for i in line.split() ])
     for initial_state in test_cases:
-        	print('From state ' + str(initial_state) + " found goal state by taking path: " + str(solve(initial_state)))
+        print('From state ' + str(initial_state) + " found goal state by taking path: " + str(solve(initial_state)))
+
+
+    
+
+
+    
+
